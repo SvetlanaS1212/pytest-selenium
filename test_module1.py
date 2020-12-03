700f1d8ea4ab312191580c4b9cc743c484e256d4
Sergey Egorov <sergey.egorov@teleste.com>
2020-12-03 14:23:19 +0200

    Initial commit

---
+++ test_module1.py
@@ -0,0 +1,48 @@
+import time
+# See https://selenium-python.readthedocs.io/getting-started.html
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def setup_function(function):
+    function.startTime = time.time()
+
+    # Open browser
+    function.driver = webdriver.Firefox()
+    # See https://selenium-python.readthedocs.io/waits.html#implicit-waits
+    function.driver.implicitly_wait(30)
+
+def teardown_function(function):
+    # Close browser
+    function.driver.quit()
+    del function.driver
+
+    duration = time.time() - function.startTime
+    print("%s: %.3f" % (function.__name__, duration))
+    del function.startTime
+
+def test_search():
+    self = test_search
+
+    # Open url
+    self.driver.get("https://duckduckgo.com/")
+
+    # Search "selenium"
+    elem = self.driver.find_element_by_name("q")
+    elem.clear()
+    elem.send_keys("selenium")
+    elem.send_keys(Keys.RETURN)
+
+    # Wait is implicit (see setup_function)
+
+    # Take 1st result url
+    elem = self.driver.find_element_by_css_selector("#r1-0 a.result__a")
+    href = elem.get_attribute("href")
+
+    # Check the result
+    assert href == "https://en.wikipedia.org/wiki/Selenium"
+
+    pass
+
+if __name__ == "__main__":
+    import pytest
+    pytest.main()
